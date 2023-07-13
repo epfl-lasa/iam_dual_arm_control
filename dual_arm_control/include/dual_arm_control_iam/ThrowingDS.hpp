2bdff6c8b7f6e3017bd9cc26eb098846c9c19b26
Elise-J <elise.jeandupeux@gmail.com>
2023-07-13 16:57:54 +0200

    adding Copyrights

--- dual_arm_control/include/dual_arm_control_iam/ThrowingDS.hpp
+++ dual_arm_control/include/dual_arm_control_iam/ThrowingDS.hpp
@@ -1,21 +1,27 @@
-/*
- * Copyright (C) 2018 Learning Algorithms and Systems Laboratory, EPFL, Switzerland
- * Authors: Mahdi Khoramshahi and Nadia Figueroa
- * email:   {mahdi.khoramshahi,nadia.figueroafernandez}@epfl.ch
- * website: lasa.epfl.ch
- *
- * This work was supported by the European Communitys Horizon 2020 Research and Innovation 
- * programme ICT-23-2014, grant agreement 644727-Cogimon and 643950-SecondHands.
- *
- * Permission is granted to copy, distribute, and/or modify this program
- * under the terms of the GNU General Public License, version 2 or any
- * later version published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details
- */
+//|
+//|    Copyright (C) 2021-2023 Learning Algorithms and Systems Laboratory, EPFL, Switzerland
+//|    Authors: Michael Bombile (maintainer)
+//|
+//|    email:   michael.bombile@epfl.ch/micbombile@gmail.com
+//|
+//|    Other contributors:
+//|             Elise Jeandupeux (elise.jeandupeux@epfl.ch)
+//|
+//|    website: lasa.epfl.ch
+//|
+//|    This file is part of iam_dual_arm_control.
+//|    This work was supported by the European Community's Horizon 2020 Research and Innovation
+//|    programme (call: H2020-ICT-09-2019-2020, RIA), grant agreement 871899 Impact-Aware Manipulation.
+//|
+//|    iam_dual_arm_control is free software: you can redistribute it and/or modify  it under the terms
+//|    of the GNU General Public License as published by  the Free Software Foundation,
+//|    either version 3 of the License, or  (at your option) any later version.
+//|
+//|    iam_dual_arm_control is distributed in the hope that it will be useful,
+//|    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//|    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//|    GNU General Public License for more details.
+//|
 
 #pragma once
 
--- dual_arm_control/include/dual_arm_control_iam/TossTaskParamEstimator.hpp
+++ dual_arm_control/include/dual_arm_control_iam/TossTaskParamEstimator.hpp
@@ -1,3 +1,28 @@
+//|
+//|    Copyright (C) 2021-2023 Learning Algorithms and Systems Laboratory, EPFL, Switzerland
+//|    Authors: Michael Bombile (maintainer)
+//|
+//|    email:   michael.bombile@epfl.ch/micbombile@gmail.com
+//|
+//|    Other contributors:
+//|             Elise Jeandupeux (elise.jeandupeux@epfl.ch)
+//|
+//|    website: lasa.epfl.ch
+//|
+//|    This file is part of iam_dual_arm_control.
+//|    This work was supported by the European Community's Horizon 2020 Research and Innovation
+//|    programme (call: H2020-ICT-09-2019-2020, RIA), grant agreement 871899 Impact-Aware Manipulation.
+//|
+//|    iam_dual_arm_control is free software: you can redistribute it and/or modify  it under the terms
+//|    of the GNU General Public License as published by  the Free Software Foundation,
+//|    either version 3 of the License, or  (at your option) any later version.
+//|
+//|    iam_dual_arm_control is distributed in the hope that it will be useful,
+//|    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//|    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//|    GNU General Public License for more details.
+//|
+
 #pragma once
 
 #include <fstream>
